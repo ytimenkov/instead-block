83e63515191b5d78761cfcd882d284d7ab5c87ac
Yuri Timenkov <yuri@timenkov.ru>
2020-09-02 19:36:49 +0000

    Adding game metadata
    Fixed zip to have folder so it can be loaded by instead.

--- src/files.ts
+++ src/files.ts
@@ -3,15 +3,23 @@ import { runGame } from "./instead";
 import JSZip = require("jszip");
 import { saveAs } from "file-saver";
 import { InsteadObject, InsteadRoom } from "./objects";
+import { GameMetaData, showInfoDialog } from "./game_info";
+import { version } from "jszip";
 
 
 export function registerFileCallbacks(workspace: WorkspaceSvg) {
     workspace.registerButtonCallback("convertToLua", (btn) => { convertOrRun(false, btn.getTargetWorkspace()); });
     workspace.registerButtonCallback("run", (btn) => { convertOrRun(true, btn.getTargetWorkspace()); });
-    workspace.registerButtonCallback("save", (btn) => { saveWorkspace(btn.getTargetWorkspace()); });
+    workspace.registerButtonCallback("save", (btn) => { backupWorkspace(btn.getTargetWorkspace()); });
     workspace.registerButtonCallback("download", (btn) => { exportProject(btn.getTargetWorkspace()); });
     workspace.registerButtonCallback("upload", (btn) => { addFileInput(btn.getTargetWorkspace()).click(); });
+    workspace.registerButtonCallback("editMetadata", (btn) => { editWorkspaceMetadata(btn.getTargetWorkspace()); });
+
     // workspace.registerToolboxCategoryCallback("INSTEAD_GAMES", inteadGamesFlyout);
+}
+
+interface WorkspaceInstead extends Workspace {
+    insteadMeta: GameMetaData;
 }
 
 /*
@@ -74,7 +82,9 @@ function resetWorkspace(workspace: Workspace) {
 }
 
 function convertOrRun(run: boolean, workspace: Workspace) {
-    const code = Lua.workspaceToCode(workspace);
+    const insteadMeta = (workspace as WorkspaceInstead).insteadMeta;
+    let code = `-- $Name: ${insteadMeta.name}$\n-- $Version: ${insteadMeta.version}$\n-- $Author: ${insteadMeta.author}$\n`;
+    code += Lua.workspaceToCode(workspace);
     const codeElem = document.getElementById("generatedCode") as HTMLElement;
     codeElem.innerText = code;
     if (run) {
@@ -90,6 +100,20 @@ export function loadWorkspace(xml: string, workspace: Workspace) {
     try {
         const dom = Xml.textToDom(xml);
         resetWorkspace(workspace);
+        // Default
+        (workspace as WorkspaceInstead).insteadMeta = { name: "Playground", version: "0.0", author: "Unknown" };
+        for (let i = 0, xmlChild; (xmlChild = dom.childNodes[i]); i++) {
+            if (xmlChild.nodeName == "instead") {
+                const attrs = (xmlChild as Element).attributes;
+                const insteadMeta: GameMetaData = {
+                    name: attrs.getNamedItem("name")?.textContent || "",
+                    version: attrs.getNamedItem("version")?.textContent || "",
+                    author: attrs.getNamedItem("author")?.textContent || "",
+                };
+                (workspace as WorkspaceInstead).insteadMeta = insteadMeta;
+                break;
+            }
+        }
         Xml.domToWorkspace(dom, workspace);
         console.log("Loaded workspace");
     } catch (error) {
@@ -100,9 +124,20 @@ export function loadWorkspace(xml: string, workspace: Workspace) {
     }
 }
 
-function saveWorkspace(workspace: Workspace) {
+function saveWorkspace(workspace: Workspace): string {
     const xml = Xml.workspaceToDom(workspace);
+    const insteadMeta = (workspace as WorkspaceInstead).insteadMeta;
+    const insteadNode = document.createElement("instead");
+    insteadNode.setAttribute("name", insteadMeta.name);
+    insteadNode.setAttribute("version", insteadMeta.version);
+    insteadNode.setAttribute("author", insteadMeta.author);
+    xml.append(insteadNode);
     const text = Xml.domToText(xml);
+    return text;
+}
+
+function backupWorkspace(workspace: Workspace) {
+    const text = saveWorkspace(workspace);
     console.log("Saving text: " + text);
     window.localStorage.setItem(localStorageKey, text);
 }
@@ -114,29 +149,29 @@ const blocksFolderName = "blocks";
 async function exportProject(workspace: Workspace) {
     const zip = new JSZip();
 
+    const gameName = (workspace as WorkspaceInstead).insteadMeta.name;
+
     // Add blocks.
-    const xml = Xml.workspaceToDom(workspace);
-    const text = Xml.domToText(xml);
-    const blocksFolder = zip.folder(blocksFolderName) as JSZip;
-    blocksFolder.file(mainFileName + ".xml", text);
+    const text = saveWorkspace(workspace);
+    zip.file(`${gameName}/${blocksFolderName}/${mainFileName}.xml`, text);
 
     // Add files to load by Instead
     const code = Lua.workspaceToCode(workspace);
-    zip.file(mainFileName + ".lua", code);
+    zip.file(`${gameName}/${mainFileName}.lua`, code);
 
     const blob = await zip.generateAsync({ type: "blob" });
-    saveAs(blob, "game.zip");
+    saveAs(blob, `${gameName}.zip`);
 }
 
 async function importProject(workspace: Workspace, input: ArrayBuffer) {
     const zip = await JSZip.loadAsync(input);
 
-    const blocksFolder = zip.folder(blocksFolderName) as JSZip;
-    const mainFile = blocksFolder.file(mainFileName + ".xml");
+    const mainFile = zip.file(new RegExp(`${blocksFolderName}/${mainFileName}.xml$`))[0];
     if (!mainFile) {
         alert("Can't find blocks definition");
         return;
     }
+
     const text = await mainFile.async("string");
     loadWorkspace(text, workspace);
 }
@@ -168,4 +203,12 @@ function addFileInput(workspace: Workspace) {
 
     document.body.appendChild(inputElem);
     return inputElem;
+}
+
+async function editWorkspaceMetadata(workspace: Workspace) {
+    const insteadMeta = (workspace as WorkspaceInstead).insteadMeta;
+    const newData = await showInfoDialog(insteadMeta);
+    if (newData) {
+        (workspace as WorkspaceInstead).insteadMeta = newData;
+    }
 }
