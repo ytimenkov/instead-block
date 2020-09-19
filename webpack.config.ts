ca56bf24f758f974bc349849e2310e66031ce34a
Yuri Timenkov <yuri@timenkov.ru>
2020-09-19 14:07:24 +0000

    Adding custom webpack builder

---
+++ webpack.config.ts
@@ -0,0 +1,51 @@
+import * as path from "path";
+import * as CopyPlugin from "copy-webpack-plugin";
+
+
+module.exports = {
+    devServer: {
+        compress: true,
+    },
+
+    resolve: {
+        alias: {
+            "instead": path.resolve(__dirname, "lib/instead"),
+            "lua.vm.js$": path.resolve(__dirname, "lib/weblua/lua.vm.js"),
+            "instead-js": path.resolve(__dirname, "lib/instead-js"),
+            "data": path.resolve(__dirname, "data"),
+        }
+    },
+
+    module: {
+        rules: [
+            { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'], },
+            { test: /.html$/, loader: "html-loader" },
+            {
+                test: /.xml$/,
+                oneOf: [
+                    { resource: /playground.xml$/, loader: "file-loader" },
+                    {
+                        loader: "html-loader",
+                        options: {
+                            esModule: true,
+                        },
+                    },
+                ]
+            },
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "lib/instead"),
+                    to: path.resolve(__dirname, "build"),
+                    globOptions: {
+                        ignore: ["*.js"]
+                    }
+                },
+            ]
+        }),
+    ],
+};
