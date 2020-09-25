64e0e6e991b15d686ffcbd173949fec64f6f64e6
Yuri Timenkov <yuri@timenkov.ru>
2020-09-25 11:12:54 +0000

    Use host binding instead of div.
    Updated properties styles according to Angular recommendations

--- src/app/instead/instead.component.ts
+++ src/app/instead/instead.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostBinding, HostListener, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { Elements } from "src/instead";
 import { InsteadService } from "../instead.service";
@@ -9,6 +9,7 @@ import { InsteadService } from "../instead.service";
   styleUrls: ["./instead.component.css"]
 })
 export class InsteadComponent implements OnInit {
+  @HostBinding("class.object-using")
   useTarget?: string;
 
   constructor(private insteadService: InsteadService, /*private sanitizer: DomSanitizer*/) { }
@@ -29,7 +30,7 @@ export class InsteadComponent implements OnInit {
     }
   }
 
-  // TODO: @HostListener('click') instead of div
+  @HostListener("click")
   use(target?: string): void {
     if (this.useTarget && this.useTarget === target) {
       this.insteadService.cmd(target);
@@ -38,19 +39,19 @@ export class InsteadComponent implements OnInit {
     }
   }
 
-  get text(): Observable<Elements[]> {
+  get text$(): Observable<Elements[]> {
     return this.insteadService.text; // .pipe(map(t => this.sanitizer.sanitize(t)));
   }
 
-  get title(): Observable<Elements[]> {
+  get title$(): Observable<Elements[]> {
     return this.insteadService.title;
   }
 
-  get ways(): Observable<Elements[]> {
+  get ways$(): Observable<Elements[]> {
     return this.insteadService.ways;
   }
 
-  get inventory(): Observable<Elements[]> {
+  get inventory$(): Observable<Elements[]> {
     return this.insteadService.inventory;
   }
 }
